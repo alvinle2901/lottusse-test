e25bb462d61f5e999848b742ae1533aefac8ffc2
alvinle2901 <bach938@gmail.com>
2025-01-17 01:09:10 +0100

    Connect to redis cloud

--- server/src/main/java/com/bl/server/config/RedisConfig.java
+++ server/src/main/java/com/bl/server/config/RedisConfig.java
@@ -1,22 +1,38 @@
 package com.bl.server.config;
 
 import com.bl.server.entity.Product;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 
 @Configuration
 @EnableCaching
 public class RedisConfig {
+    @Value("${spring.data.redis.host}")
+    private String hostName;
+
+    @Value("${spring.data.redis.port}")
+    private int port;
+
+    @Value("${spring.data.redis.password}")
+    private String password;
+
     @Bean
-    public org.springframework.data.redis.connection.RedisConnectionFactory redisConnectionFactory() {
-        return new org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory();
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        redisStandaloneConfiguration.setPassword(password);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
     public org.springframework.data.redis.core.RedisTemplate<String, Product> redisTemplate() {
         org.springframework.data.redis.core.RedisTemplate<String, Product> template = new org.springframework.data.redis.core.RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory());
+        template.setConnectionFactory(lettuceConnectionFactory());
         return template;
     }
 }
